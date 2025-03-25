4e8a45a70a22d68d15b1a5798b130d7dbc43de8d
mike <burkovmu@gmail.com>
2025-03-26 02:12:44 +0500

    fix: обновлена конфигурация ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  extends: 'next/core-web-vitals',
   rules: {
     '@typescript-eslint/no-unused-vars': 'off',
     'react-hooks/exhaustive-deps': 'off'
